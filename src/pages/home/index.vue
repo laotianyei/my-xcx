84fdc41d366c3654f305003ffd32076c96a4268b
libinbin <1452608242@qq.com>
2019-05-19 00:09:39 +0800

    项目搭建

---
+++ src/pages/home/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <p class="a">
+      <button @click="aaa(index)" v-for="(item,index) in listData" :key="index">{{item.title}}</button>
+    </p>
+
+    <div :class="{active:index1==index}" v-for="(item,index)in listData" :key="item.id">{{item.content}}
+        {{index1}}
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        index1:1,
+      listData: [
+        { id: 1, title: "apple", content: "苹果" },
+        { id: 2, title: "orange", content: "橘子" },
+        { id: 3, title: "lemon", content: "柠檬" }
+      ]
+    };
+  },
+  methods: {
+    aaa(index) {
+   
+      this.index1 = index;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.a {
+  display: flex;
+}
+.a button {
+  flex: 1;
+}
+div div {
+  width: 200px;
+  height: 200px;
+  background: skyblue;
+  display: none;
+}
+div .active {
+  display: block;
+}
+</style>
